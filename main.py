a1224fd3787e802060103548f6b2c09e9bff30a1
Patrick <patrickbjerre@outlook.dk>
2024-04-08 10:40:44 +0200

    made the initial CLI tool

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
 #!/usr/bin/env python3
 import os
+import pyfiglet
+import cmd
 
-try:
-    os.system("figlet -c SystemSurge")
-except:
-    pass
+
+class CLI(cmd.Cmd):
+
+    prompt = '>> '
+    intro = "Welcome to SystemSurge"
+
+    def do_hello(self, line):
+        """Print a greeting."""
+        print("Hello, World!")
+
+    def do_quit(self, line):
+        """Exit the CLI."""
+        return True
+
+
+if __name__ == '__main__':
+    try:
+        ascii_banner = pyfiglet.figlet_format("SystemSurge")
+        print(ascii_banner)
+    except:
+        pass
+
+
+    CLI().cmdloop()
